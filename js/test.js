4eb88dbb16440bfc7f87feccf780329026d88cb7
patronpenguin <xh5277@gmail.com>
2015-09-19 16:22:29 -0700

    for merge

--- js/test.js
+++ js/test.js
@@ -1,3 +1,9 @@
-window.onload = function() {
-    alert('loaded');
-}
+$(window).scroll(
+    function() {
+        if ($(window).scrollTop() > 200) {
+            $('#header').attr('class', 'float');
+        } else {
+            $('#header').attr('class', 'normal');
+        }
+    }
+);
